b07fb7b7e8f1daf9db211f179e87288cdcfb2eb5
bruktawit mekuria <buktawit0101@gmail.com>
2023-04-21 18:34:15 -0700

    verify user is signed in

---
+++ LocalHost_store/src/pages/MyAccountSignedIn.py
@@ -0,0 +1,21 @@
+
+from LocalHost_store.src.pages.Locators.MyAccountSignedInLocator import MyAccountSignedInLocators
+from LocalHost_store.src.selenium_extended.SeleniumExtended import SeleniumExtended
+
+class MyAccountSignedIn(MyAccountSignedInLocators):
+    endpoint = '/my_account/'
+
+    def __init__(self, driver):
+        self.driver = driver
+        self.sl = SeleniumExtended(self.driver)
+
+    def verify_user_is_signed_in(self):
+       '''
+       Verifies user is digned by check the 'logout' is visible
+       on the left navigation bar.
+       :return:
+       '''
+       self.sl.wait_until_element_is_visible(self.LEFT_NAV_LOGOUT_BTN)
+
+
+
---
+++ LocalHost_store/tests/frontend/my_account/test_user_valid_login.py
@@ -0,0 +1,23 @@
+import pytest
+from LocalHost_store.src.pages.MyAccountSignedOut import MyAccountSignedOut
+from LocalHost_store.src.pages.MyAccountSignedIn import MyAccountSignedIn
+# from LocalHost_store.src.helpers.generic_helpers import generate_random_email_and_password
+
+pytestmarks = [pytest.mark.fe, pytest.mark.regression, pytest.mark.smoke, pytest.mark.my_account]
+@pytest.mark.usefixtures("init_driver")
+
+class TestUserValidLogin:
+    @pytest.mark.smoke
+    @pytest.mark.qactcid1
+    def test_user_valid_login(self):
+
+
+        my_account_o = MyAccountSignedOut(self.driver)
+        my_account_I = MyAccountSignedIn(self.driver)
+        my_account_o.go_to_my_account()
+        my_account_o.input_login_username('QAtestusername@gmail.com')
+        my_account_o.input_login_password('Regpassword@1')
+        my_account_o.click_on_login_btn()
+        my_account_I.verify_user_is_signed_in()
+
+        breakpoint()
